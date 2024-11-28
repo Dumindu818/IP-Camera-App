257ea326d6b845b7f37ff1764c240e169e532f53
Dumindu de Silva <dumindudesilva1@gmail.com>
2024-11-28 09:27:50 +0530

    Add project files.

---
+++ Home.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace IP_Camera_App
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStopRecording = new System.Windows.Forms.Button();
+            this.btnStartRecording = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStopRecording
+            // 
+            this.btnStopRecording.BackColor = System.Drawing.Color.Crimson;
+            this.btnStopRecording.Font = new System.Drawing.Font("Poppins", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStopRecording.ForeColor = System.Drawing.Color.White;
+            this.btnStopRecording.Location = new System.Drawing.Point(511, 562);
+            this.btnStopRecording.Name = "btnStopRecording";
+            this.btnStopRecording.Size = new System.Drawing.Size(443, 124);
+            this.btnStopRecording.TabIndex = 9;
+            this.btnStopRecording.Text = "Stop Recording";
+            this.btnStopRecording.UseVisualStyleBackColor = false;
+            this.btnStopRecording.Click += new System.EventHandler(this.btnStopRecording_Click);
+            // 
+            // btnStartRecording
+            // 
+            this.btnStartRecording.BackColor = System.Drawing.Color.Green;
+            this.btnStartRecording.Font = new System.Drawing.Font("Poppins", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartRecording.ForeColor = System.Drawing.Color.White;
+            this.btnStartRecording.Location = new System.Drawing.Point(24, 562);
+            this.btnStartRecording.Name = "btnStartRecording";
+            this.btnStartRecording.Size = new System.Drawing.Size(457, 124);
+            this.btnStartRecording.TabIndex = 8;
+            this.btnStartRecording.Text = "Start Recording";
+            this.btnStartRecording.UseVisualStyleBackColor = false;
+            this.btnStartRecording.Click += new System.EventHandler(this.btnStartRecording_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnStart.Font = new System.Drawing.Font("Poppins", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnStart.Location = new System.Drawing.Point(24, 262);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(251, 41);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start Streaming";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pictureBox.Location = new System.Drawing.Point(24, 92);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(930, 445);
+            this.pictureBox.TabIndex = 6;
+            this.pictureBox.TabStop = false;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(24, 209);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(251, 27);
+            this.txtUrl.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Poppins ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(1200, 15);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(63, 34);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(19, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 30);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter IP address of Camera :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Britannic Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(27, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 44);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "IP Camcorder";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Location = new System.Drawing.Point(970, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 594);
+            this.panel1.TabIndex = 13;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStopRecording);
+            this.Controls.Add(this.btnStartRecording);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStopRecording;
+        private System.Windows.Forms.Button btnStartRecording;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Home.cs
@@ -0,0 +1,281 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Accord.Video;
+using Accord.Video.FFMPEG;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace IP_Camera_App
+{
+    public partial class Home : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopTect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        private MJPEGStream videoSource;
+        private VideoFileWriter videoWriter;
+        private bool isRecording = false;
+        private string outputPath;
+        private int recordingCount = 0;
+        private readonly object lockObject = new object();
+
+        public Home()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text.Trim();
+
+            // Check if the TextBox is empty
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                MessageBox.Show("Please enter the IP camera URL.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Add "http://" to the beginning if it's missing
+            if (!url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) &&
+                !url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            {
+                url = "http://" + url;
+            }
+
+            // Validate the URL format (for IP address and optional port)
+            string ipPattern = @"^(http:\/\/|https:\/\/)?(\d{1,3}\.){3}\d{1,3}(:\d+)?(\/.*)?$";
+            if (!System.Text.RegularExpressions.Regex.IsMatch(url, ipPattern))
+            {
+                MessageBox.Show("Please enter a valid IP camera URL (e.g., http://192.168.1.100:8080).", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Create the MJPEGStream object
+                videoSource = new MJPEGStream(url);
+
+                // Test the stream to ensure it provides frames
+                bool streamAvailable = TestStream(videoSource);
+
+                if (streamAvailable)
+                {
+                    // Subscribe to NewFrame event and start the stream
+                    videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                    videoSource.Start();
+                    MessageBox.Show("Video stream started successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("The provided URL is not streaming video. Please check the camera setup.", "Stream Unavailable", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle errors when trying to start the stream
+                MessageBox.Show($"Failed to start the video stream. Please check the IP address and ensure the camera is accessible.\n\nError: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        // Method to test the video stream
+        private bool TestStream(MJPEGStream stream)
+        {
+            bool frameReceived = false;
+
+            // Temporary event handler to capture a frame
+            NewFrameEventHandler handler = (sender, eventArgs) =>
+            {
+                frameReceived = true;
+                stream.SignalToStop(); // Stop the stream after receiving a frame
+            };
+
+            try
+            {
+                stream.NewFrame += handler;
+                stream.Start();
+
+                // Wait briefly to see if a frame is received
+                DateTime timeout = DateTime.Now.AddSeconds(5);
+                while (!frameReceived && DateTime.Now < timeout)
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            finally
+            {
+                stream.SignalToStop();
+                stream.WaitForStop();
+                stream.NewFrame -= handler; // Remove the temporary handler
+            }
+
+            return frameReceived;
+        }
+
+
+
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = null;
+            lock (lockObject)
+            {
+                bitmap = (Bitmap)eventArgs.Frame.Clone();
+                if (isRecording && videoWriter != null)
+                {
+                    videoWriter.WriteVideoFrame(bitmap);
+                }
+            }
+
+            UpdatePictureBox(bitmap);
+        }
+        private void UpdatePictureBox(Bitmap bitmap)
+        {
+            if (pictureBox.InvokeRequired)
+            {
+                pictureBox.Invoke(new Action<Bitmap>(UpdatePictureBox), bitmap);
+            }
+            else
+            {
+                // Dispose of the old image if it exists
+                if (pictureBox.Image != null)
+                {
+                    pictureBox.Image.Dispose();
+                }
+
+                // Set the new image
+                pictureBox.Image = bitmap;
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (isRecording)
+            {
+                // Stop recording
+                isRecording = false;
+
+                lock (lockObject)
+                {
+                    if (videoWriter != null)
+                    {
+                        videoWriter.Close();
+                        videoWriter = null;
+                    }
+                }
+
+                // Stop the video source
+                if (videoSource != null && videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource.WaitForStop();
+                }
+
+                // Now show the message and exit
+                MessageBox.Show($"Recording stopped and saved to {outputPath}", "Recording Stopped", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Exit the application
+            Application.Exit();
+        }
+
+
+
+        private void btnStartRecording_Click(object sender, EventArgs e)
+        {
+            if (videoSource == null || !videoSource.IsRunning)
+            {
+                MessageBox.Show("Please start the video stream first.");
+                return;
+            }
+
+            // Generate a unique filename based on the current date, time, and sequence number
+            string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string sequenceNumber = (++recordingCount).ToString("D3"); // Format as a 3-digit number
+            string filename = $"recording_{timestamp}_{sequenceNumber}.avi";
+
+            
+            
+            // Get the path to the user's Downloads folder
+            string downloadsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+
+            outputPath = Path.Combine(downloadsFolder, filename);
+
+            videoWriter = new VideoFileWriter();
+            videoWriter.Open(outputPath, pictureBox.Width, pictureBox.Height, 25, VideoCodec.MPEG4, 1000000);
+            MessageBox.Show("Recording is Started");
+            isRecording = true;
+        }
+
+        private void btnStopRecording_Click(object sender, EventArgs e)
+        {
+            // Check if the video stream is active
+            if (videoSource == null || !videoSource.IsRunning)
+            {
+                MessageBox.Show("Please start the video stream first.");
+                return;
+            }
+
+            // Check if recording is active
+            if (!isRecording)
+            {
+                MessageBox.Show("Recording is not in progress. Please start the recording first.");
+                return;
+            }
+
+            // Stop recording
+            isRecording = false;
+            lock (lockObject)
+            {
+                videoWriter.Close();
+            }
+            videoWriter = null;
+            MessageBox.Show($"Recording stopped and saved to {outputPath}");
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Stop the video source if it's running
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+
+            // Stop recording if it is in progress
+            if (isRecording)
+            {
+                isRecording = false;
+                lock (lockObject)
+                {
+                    videoWriter.Close();
+                }
+                videoWriter = null;
+                MessageBox.Show($"Recording stopped and saved to {outputPath}");
+            }
+
+            base.OnFormClosing(e);
+        }
+
+    }
+}
